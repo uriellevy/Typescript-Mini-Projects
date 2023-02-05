eab096919fffb34c3e7fc25df21d835a8027e51d
uriellevy <uriellevy4892@gmail.com>
2023-02-05 14:25:46 +0200

    digital clock implemented

---
+++ digitalClock/clock.ts
@@ -0,0 +1,25 @@
+const clock = document.querySelector(".clock") as Element;
+let interval;
+
+window.onload = () => {
+    setClock();
+}
+
+// const setClock = () => {//easy way
+//     interval = setInterval(() => {
+//         const date = new Date().toLocaleTimeString()
+//         clock.innerHTML = date;
+//     },1000)
+// }
+
+const setClock = () => {//manual way
+    interval = setInterval(() => {
+        const date = new Date();
+        const hh = date.getHours();
+        const mm = date.getMinutes();
+        const ss = date.getSeconds();
+        const amPm = hh < 12 ? "AM" : "PM";
+
+        clock.innerHTML = `${hh}:${mm}:${ss} ${amPm}`
+    },1000)
+}
